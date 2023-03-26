bee14c442e03ec4b781ba999609aa741650a61d9
Hung Nguyen <hungkojno1@gmail.com>
2023-03-26 21:57:31 +0700

    trang cart them nut thanh toan

--- src/pages/user/Cart/index.jsx
+++ src/pages/user/Cart/index.jsx
@@ -9,24 +9,28 @@ import styles from './css/cart.module.scss';
 
 import { Link } from 'react-router-dom';
 import CartItem from './cart-item';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Cart() {
-    const listItem = [
-        {
-            img: 'https://i.pinimg.com/236x/d2/a6/18/d2a618aeee3bbf580164d25babe74a29.jpg',
-            name: 'MR JUICER - MADX BULL ( TĂNG LỰC LẠNH ) - SALT \n NICOTINE',
-            price: 120000,
-            quantity: 2,
-        },
-        {
-            img: 'https://i.pinimg.com/236x/d2/a6/18/d2a618aeee3bbf580164d25babe74a29.jpg',
-            name: 'MR JUICER - MADX BULL ( TĂNG LỰC LẠNH ) - SALT \n NICOTINE',
-            price: 120000,
-            quantity: 2,
-        },
-    ];
+    const [data, setData] = useState({
+        listItem: [
+            {
+                img: 'https://i.pinimg.com/236x/d2/a6/18/d2a618aeee3bbf580164d25babe74a29.jpg',
+                name: 'MR JUICER - MADX BULL ( TĂNG LỰC LẠNH ) - SALT \n NICOTINE',
+                price: 120000,
+                quantity: 2,
+            },
+            {
+                img: 'https://i.pinimg.com/236x/d2/a6/18/d2a618aeee3bbf580164d25babe74a29.jpg',
+                name: 'MR JUICER - MADX BULL ( TĂNG LỰC LẠNH ) - SALT \n NICOTINE',
+                price: 120000,
+                quantity: 2,
+            },
+        ],
+        total: 480000,
+    });
     return (
         <div className={cx('cart')}>
             <Container className={cx(['d-block', 'mh-0'])}>
@@ -55,9 +59,33 @@ function Cart() {
                         </Col>
                     </Row>
 
-                    {listItem.map((item, index) => (
+                    {data.listItem.map((item, index) => (
                         <CartItem key={index} product={item} />
                     ))}
+                </div>
+                <div className={cx(['total-bill'])}>
+                    <Row
+                        className={cx(['p-0'])}
+                        style={{ justifyContent: 'end' }}
+                    >
+                        <Col sm={4} className={cx(['p-0'])}>
+                            <div className={cx(['total-price'])}>
+                                <span>Tổng tiền:</span>
+                                <span className={cx(['price'])}>
+                                    {Number(data.total).toLocaleString(
+                                        'vi-VN',
+                                        {
+                                            style: 'currency',
+                                            currency: 'VND',
+                                        }
+                                    )}
+                                </span>
+                            </div>
+                            <div className={cx(['pay-button'])}>
+                                <Link>Thanh toán</Link>
+                            </div>
+                        </Col>
+                    </Row>
                 </div>
             </Container>
         </div>
