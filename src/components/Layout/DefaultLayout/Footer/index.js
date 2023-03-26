2ace21c97f2d52b5d82e304b23680c4f9dee9fd8
Dat Dang <datd61340@gmail.com>
2023-03-26 21:17:31 +0700

    update link in footer

--- src/components/Layout/DefaultLayout/Footer/index.js
+++ src/components/Layout/DefaultLayout/Footer/index.js
@@ -13,17 +13,17 @@ function Footer() {
                     <div className="col-md-4 footerCol">
                         <div className="footerTitle">Liên hệ</div>
                         <ul>
-                            <li>Địa chỉ: Ha Noi</li>
-                            <li>Số điện thoại: <Link to={"tel:0963632312"}>0963632312</Link></li>
-                            <li>Email: <Link to={"mailto:dangminhdat@minhdat.dev"}>dangminhdat@minhdat.dev</Link></li>
+                            <li>Địa chỉ: <Link className='footer-link' to={"/contact"}>Ha Dong, Ha Noi</Link></li>
+                            <li>Số điện thoại: <Link className='footer-link' to={"tel:0963632312"}>0963632312</Link></li>
+                            <li>Email: <Link className='footer-link' to={"mailto:dangminhdat@minhdat.dev"}>dangminhdat@minhdat.dev</Link></li>
                         </ul>
                     </div>
                     <div className="col-md-4 footerCol">
                         <div className="footerTitle">Về chúng tôi</div>
                         <ul>
                             <li>Về chúng tôi</li>
-                            <li><Link to={"#"}>Chính sách thanh toán</Link></li>
-                            <li><Link to={"#"}>Chính sách thanh đổi trả</Link></li>
+                            <li><Link className='footer-link' to={"#"}>Chính sách thanh toán</Link></li>
+                            <li><Link className='footer-link' to={"#"}>Chính sách thanh đổi trả</Link></li>
                         </ul>
                     </div>
                 </div>
