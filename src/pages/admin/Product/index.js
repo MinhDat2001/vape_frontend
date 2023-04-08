5839c6a8d05d8d5c73daacd5976cf4acfc32696c
Hung Nguyen <hungkojno1@gmail.com>
2023-04-08 12:02:24 +0700

    create route

--- src/pages/admin/Product/index.js
+++ src/pages/admin/Product/index.js
@@ -1,9 +1,14 @@
-import logo from '~/static/images/logo.png'
-import './product.css'
+import { Link } from 'react-router-dom';
+import logo from '~/static/images/logo.png';
+import './product.css';
 function Product() {
-    var productContent = (
+    return (
         <div className="product_content">
-            <div className='contain_add'><button>Thêm</button></div>
+            <div className="contain_add">
+                <Link to={'/admin/product/add'} className="admin-page-btn">
+                    Thêm
+                </Link>
+            </div>
             <table className="product_table">
                 <thead>
                     <tr>
@@ -19,150 +24,36 @@ function Product() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
+                        <td width="12%">
+                            <img
+                                className="product_img"
+                                src={logo}
+                                alt="product"
+                            />
+                        </td>
                         <td width="12%">S1200</td>
                         <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
+                        <td width="8%">
+                            <input type="number" value="100" readOnly />
+                        </td>
                         <td width="12%">12000 vnđ</td>
                         <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
-                    </tr>
-                    <tr>
-                        <td width="12%"><img className='product_img' src={logo} alt = "product"/></td>
-                        <td width="12%">S1200</td>
-                        <td width="24%">Ngon bổ rẻ</td>
-                        <td width="8%"><input type="number" value="100" readOnly/></td>
-                        <td width="12%">12000 vnđ</td>
-                        <td width="12%">Chất lượng cao</td>
-                        <td width="10%"><button>Chỉnh sửa</button></td>
-                        <td width="10%"><button>Xóa</button></td>
+                        <td width="10%">
+                            <Link
+                                to={'/admin/product/1'}
+                                className="admin-page-btn"
+                            >
+                                Chỉnh sửa
+                            </Link>
+                        </td>
+                        <td width="10%">
+                            <Link className="admin-page-btn">Xóa</Link>
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </div>
-    )
-    return productContent
+    );
 }
 
-export default Product
+export default Product;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,32 @@
-import { default as userHome } from '~/pages/user/Home'
-import Shop from '~/pages/user/Shop'
-import Product from '~/pages/user/Product'
-import Profile from '~/pages/user/Profile'
-import Payment from '~/pages/user/Payment'
-import Category from '~/pages/user/Category'
-import Cart from '~/pages/user/Cart'
-import { default as userLogin } from '~/pages/user/Login'
-import { default as userRegisterAuth } from '~/pages/user/RegisterAuth'
-import { default as userRegister } from '~/pages/user/Register'
+import { default as userHome } from '~/pages/user/Home';
+import Shop from '~/pages/user/Shop';
+import Product from '~/pages/user/Product';
+import Profile from '~/pages/user/Profile';
+import Payment from '~/pages/user/Payment';
+import Category from '~/pages/user/Category';
+import Cart from '~/pages/user/Cart';
+import { default as userLogin } from '~/pages/user/Login';
+import { default as userRegisterAuth } from '~/pages/user/RegisterAuth';
+import { default as userRegister } from '~/pages/user/Register';
 
-import { default as adminHome } from '~/pages/admin/Home'
-import { default as adminLogin } from '~/pages/admin/Login'
-import { default as adminProduct } from '~/pages/admin/Product'
-import { default as adminCategory } from '~/pages/admin/Category'
-import Setting from '~/pages/admin/Setting'
+import { default as adminHome } from '~/pages/admin/Home';
+import { default as adminLogin } from '~/pages/admin/Login';
+import { default as adminProduct } from '~/pages/admin/Product';
+import { default as adminCategory } from '~/pages/admin/Category';
+import Setting from '~/pages/admin/Setting';
 
-import { DefaultLayout, AdminLayout } from '~/components/Layout'
-import Contact from '~/pages/user/Contact'
+import { DefaultLayout, AdminLayout } from '~/components/Layout';
+import Contact from '~/pages/user/Contact';
 
 const userRoute = [
     { path: '', component: userHome, Layout: DefaultLayout },
     { path: '/login', component: userLogin, Layout: DefaultLayout },
     { path: '/register', component: userRegister, Layout: DefaultLayout },
-    { path: '/register/auth', component: userRegisterAuth, Layout: DefaultLayout },
+    {
+        path: '/register/auth',
+        component: userRegisterAuth,
+        Layout: DefaultLayout,
+    },
     { path: '/product', component: Shop, Layout: DefaultLayout },
     {
         path: '/product/{product-slug}',
@@ -34,13 +38,33 @@ const userRoute = [
     { path: '/category', component: Category, Layout: DefaultLayout },
     { path: '/cart', component: Cart, Layout: DefaultLayout },
     { path: '/payment', component: Payment, Layout: DefaultLayout },
-]
+];
 const adminRoute = [
     { path: '/admin', component: adminHome, Layout: AdminLayout },
     { path: '/admin/login', component: adminLogin, Layout: null },
     { path: '/admin/product', component: adminProduct, Layout: AdminLayout },
+    {
+        path: '/admin/product/:productId',
+        component: adminProduct,
+        Layout: AdminLayout,
+    },
+    {
+        path: '/admin/product/add',
+        component: adminProduct,
+        Layout: AdminLayout,
+    },
     { path: '/admin/category', component: adminCategory, Layout: AdminLayout },
+    {
+        path: '/admin/category/:categoryId',
+        component: adminCategory,
+        Layout: AdminLayout,
+    },
+    {
+        path: '/admin/category/add',
+        component: adminCategory,
+        Layout: AdminLayout,
+    },
     { path: '/admin/setting', component: Setting, Layout: AdminLayout },
-]
+];
 
-export { adminRoute, userRoute }
+export { adminRoute, userRoute };
