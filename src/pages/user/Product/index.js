e228305a6c200a1618c42a66a6e2e952e20e7985
Cuu-thieu-nhi <comchaychancanh@gmail.com>
2023-04-09 21:36:47 +0700

    thêm ảnh, bình luận

--- src/pages/user/Product/index.js
+++ src/pages/user/Product/index.js
@@ -1,12 +1,19 @@
-import ProductImageBlock from './product-image-block'
 import ProductContentBlock from './product-content-block'
+import ProductImageGallery from './product-image-gallery'
 import React from 'react'
 import './styles.css'
 
 function Product() {
+    const images = [
+        { src: 'https://picsum.photos/id/1018/300/500/', alt: 'Đỏ' },
+        { src: 'https://picsum.photos/id/1015/300/500/', alt: 'Vàng' },
+        { src: 'https://picsum.photos/id/1019/300/500/', alt: 'Đen' },
+        { src: 'https://picsum.photos/id/1016/300/500/', alt: 'Xanh' },
+    ]
+
     return (
         <div className="main">
-            <ProductImageBlock />
+            <ProductImageGallery images={images} />
             <ProductContentBlock />
         </div>
     )
