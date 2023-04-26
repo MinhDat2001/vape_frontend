f087e917554930aba23ef41a9fc84fa33780eab3
Hung Nguyen <hungkojno1@gmail.com>
2023-04-26 13:45:18 +0700

    get user admin

---
+++ src/pages/admin/User/api.jsx
@@ -0,0 +1,5 @@
+export const USER_GET_ALL = 'http://localhost:8088/user/getAll';
+
+export default {
+    USER_GET_ALL,
+};
--- src/pages/admin/User/index.jsx
+++ src/pages/admin/User/index.jsx
@@ -1,23 +1,51 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '~/static/images/logo.png';
+import { USER_GET_ALL } from './api';
+
 function User() {
     const [users, setUsers] = useState([
         {
             id: 1,
             avatar: logo,
-            username: 'abc',
+            username: 'abc1',
             password: 'abc',
-            role: 'abc',
+            role: ['user'],
+            name: 'Hùng',
+            phone: '123456',
         },
         {
             id: 2,
             avatar: logo,
-            username: 'abc',
+            username: 'abc2',
             password: 'abc',
-            role: 'abc',
+            role: ['admin'],
+            name: 'Trang',
+            phone: '12434',
         },
     ]);
+
+    const token =
+        'Vape ' +
+        document.cookie
+            .split('; ')
+            .find((row) => row.startsWith('token='))
+            ?.split('=')[1];
+
+    useEffect(() => {
+        if (token !== undefined || token !== null || token.trim() !== '') {
+            axios
+                .get(USER_GET_ALL, { headers: { token: token } })
+                .then((response) => {
+                    setUsers(response.data.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, []);
 
     return (
         <div className="product_content">
@@ -42,9 +70,11 @@ function User() {
             <table className="product_table">
                 <thead>
                     <tr>
-                        <th></th>
+                        <th>id</th>
+                        <th>Ảnh</th>
                         <th>Tài khoản</th>
-                        <th>Mật khẩu</th>
+                        <th>Tên</th>
+                        <th>Số điện thoại</th>
                         <th>Loại thành viên</th>
                         <th></th>
                         <th></th>
@@ -53,6 +83,9 @@ function User() {
                 <tbody>
                     {users.map((item, index) => (
                         <tr key={index}>
+                            {/* id */}
+                            <td width="10%">{item.id}</td>
+                            {/* avatar */}
                             <td width="12%">
                                 <img
                                     className="product_img"
@@ -61,9 +94,9 @@ function User() {
                                 />
                             </td>
                             <td width="12%">{item.username}</td>
-                            <td width="12%">{item.password}</td>
-                            <td width="24%">{item.role}</td>
-
+                            <td width="12%">{item.name}</td>
+                            <td width="12%">{item.phone}</td>
+                            <td width="12%">{item.role}</td>
                             <td width="10%">
                                 <Link
                                     to={'/admin/user/' + item.id}
