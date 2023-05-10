b51b7f44061001b2fc026637f5f7c7c75de3fc3f
Hung Nguyen <hungkojno1@gmail.com>
2023-05-10 14:34:06 +0700

    my commit 10/5

--- src/pages/user/Product/product-content-block.js
+++ src/pages/user/Product/product-content-block.js
@@ -7,32 +7,37 @@ import { getProduct } from '~/pages/Host';
 import axios from 'axios';
 
 function ProductContentBlock() {
-    var id = window.location.pathname.split('/')[2]
+    var id = window.location.pathname.split('/')[2];
     const [product, setProduct] = useState({});
-    useEffect(()=> function callUser(){
-        axios.get((getProduct+id), {
-            mode: 'cors',
-            headers: {
+    useEffect(
+        () =>
+            function callUser() {
+                axios
+                    .get(getProduct + id, {
+                        mode: 'cors',
+                        headers: {},
+                    })
+                    .then((response) => {
+                        if (response.data.status === 0) {
+                            setProduct(response.data.data);
+                        } else {
+                        }
+                    })
+                    .catch(function (error) {});
             },
-        })
-        .then((response) => {
-            if (response.data.status === 0) {
-                setProduct(response.data.data)
-            } else {
-            }
-        })
-        .catch(function (error) {
-        })
-        
-    },[])
-    
+        []
+    );
 
-    const userName = 'Do Danh Tuan';
+    const userName = 'hung';
 
     return (
         <div className="product-content">
             <ProductContentHeader name={product.name} />
-            <ProductContentDes2 price={product.price} quantity={product.quantity} data={product.description} />
+            <ProductContentDes2
+                price={product.price}
+                quantity={product.quantity}
+                data={product.description}
+            />
             <ProductReview username={userName} />
         </div>
     );
